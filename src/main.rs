ee55237af301f344ab3884807d604cf42bc7dcd7
0/0 <zero@imaskeleton.me>
2022-04-17 13:51:28 -0600

    import stuff and fix typos

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use std::sync::Arc;
-
 use axum::{
-    extract::{ConnectInfo, Multipart, Path, TypedHeader},
-    headers::ContentLength,
+    body,
+    extract::{
+        multipart::{self, Multipart},
+        ConnectInfo, Path, TypedHeader,
+    },
+    headers::{ContentLength, HeaderMap},
+    http::{
+        header::{self, HeaderValue},
+        StatusCode,
+    },
+    response::{IntoResponse, Response},
     routing::get,
 };
+use chrono::{DateTime, Local};
+use dashmap::DashMap;
+use parking_lot::RwLock;
+use sqlx::PgPool;
+use std::{
+    borrow::Cow,
+    collections::BinaryHeap,
+    net::SocketAddr,
+    sync::Arc,
+    time::{Duration, Instant},
+};
+use tera::Tera;
+use tokio::time::sleep;
 
 #[macro_use]
 extern crate sqlx;
@@ -24,14 +44,12 @@ struct Config {
 #[derive(Clone, Debug)]
 struct State {
     config: Config,
-    db: sqlx::PgPool,
+    db: PgPool,
 }
 
 struct Cache {
-    data: dashmap::DashMap<String, String>,
-    expiries: parking_lot::RwLock<
-        std::collections::BinaryHeap<(chrono::DateTime<chrono::Local>, String)>,
-    >
+    data: DashMap<String, String>,
+    expire_timestamps: RwLock<BinaryHeap<(DateTime<Local>, String)>>,
 }
 
 #[tokio::main]
@@ -53,7 +71,7 @@ async fn main() {
     let config_string = std::fs::read_to_string(&cfg_path).expect("Failed to read config");
     let config = toml::from_str::<Config>(&config_string).expect("Failed to parse config");
 
-    let mut tera = tera::Tera::default();
+    let mut tera = Tera::default();
     tera.add_raw_template("paste.html", include_str!("./paste.html"))
         .expect("Failed to load paste.html as template");
     let pool = sqlx::postgres::PgPoolOptions::new()
@@ -62,15 +80,14 @@ async fn main() {
         .await
         .expect("Failed to connect to database!");
     migrate!("./migrations").run(&pool).await.unwrap();
-    let ratelimits: Arc<dashmap::DashMap<String, std::time::Instant>> =
-        Arc::new(dashmap::DashMap::new());
+    let ratelimit_map: Arc<DashMap<String, Instant>> = Arc::new(DashMap::new());
     let state = State {
         config: config.clone(),
         db: pool,
     };
     let cache: Arc<Cache> = Arc::new(Cache {
-        data: dashmap::DashMap::new(),
-        expiries: parking_lot::RwLock::new(std::collections::BinaryHeap::new()),
+        data: DashMap::new(),
+        expire_timestamps: RwLock::new(BinaryHeap::new()),
     });
     let add_state = state.clone();
     let view_state = state.clone();
@@ -80,27 +97,27 @@ async fn main() {
     let app = axum::Router::new()
         .route(
             "/",
-            get(root).post(move |typedheader, multipart, headers, addr| {
+            get(root).post(move |th, multipart, headers, addr| {
                 submit(
-                    typedheader,
+                    th,
                     multipart,
                     headers,
                     addr,
                     add_state,
                     add_cache,
-                    ratelimits,
+                    ratelimit_map,
                 )
             }),
         )
         .route(
             "/:path",
-            get(move |id| getpaste(id, view_state, view_cache, tera)),
+            get(move |id| get_paste(id, view_state, view_cache, tera)),
         );
     tokio::spawn(async move { delete_expired(&deleter_state.db).await });
     tokio::spawn(async move { clear_cache(cache, config.cache).await });
     warn!("Listening on http://0.0.0.0:{} (http)", config.port);
-    axum::Server::bind(&std::net::SocketAddr::from(([0, 0, 0, 0], config.port)))
-        .serve(app.into_make_service_with_connect_info::<std::net::SocketAddr>())
+    axum::Server::bind(&SocketAddr::from(([0, 0, 0, 0], config.port)))
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .expect("Failed to bind to address, is something else using the port?");
 }
@@ -110,12 +127,12 @@ axum_static_macro::static_file!(root, "index.html", axum_static_macro::content_t
 async fn submit(
     TypedHeader(length): TypedHeader<ContentLength>,
     mut multipart: Multipart,
-    headers: axum::headers::HeaderMap,
-    ConnectInfo(addr): ConnectInfo<std::net::SocketAddr>,
+    headers: HeaderMap,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
     state: State,
     cache: Arc<Cache>,
-    ratelimits: Arc<dashmap::DashMap<String, std::time::Instant>>,
-) -> Result<(axum::http::StatusCode, axum::http::HeaderMap, String), Error> {
+    ratelimit_map: Arc<DashMap<String, Instant>>,
+) -> Result<(StatusCode, HeaderMap, String), Error> {
     if let Some(wait_time) = state.config.ratelimit {
         let remote: String;
         if let Some(remote_ip) = headers.get("X_REAL_IP") {
@@ -123,19 +140,19 @@ async fn submit(
         } else {
             remote = addr.ip().to_string()
         }
-        if let Some(rl) = ratelimits.get(&remote) {
+        if let Some(rl) = ratelimit_map.get(&remote) {
             let last_paste = rl.value();
-            if let Some(time_until_unlimited) = std::time::Duration::from_secs(wait_time)
+            if let Some(time_until_unlimited) = Duration::from_secs(wait_time)
                 .checked_sub(last_paste.elapsed())
                 .map(|x| x.as_secs())
             {
                 return Err(Error::RateLimited(time_until_unlimited));
             }
         }
-        ratelimits.insert(remote, std::time::Instant::now());
+        ratelimit_map.insert(remote, Instant::now());
     }
 
-    println!("{:?}", ratelimits);
+    println!("{:?}", ratelimit_map);
     if length.0 > state.config.size_limit.unwrap_or(1024) * 1024 {
         return Err(Error::PasteTooLarge);
     }
@@ -148,7 +165,7 @@ async fn submit(
     }
 
     let persistence_length = chrono::Duration::weeks(1);
-    let expires = chrono::offset::Local::now()
+    let expires = Local::now()
         .checked_add_signed(persistence_length)
         .ok_or(Error::TimeError)?;
     let key = random_string::generate(
@@ -166,28 +183,24 @@ async fn submit(
     .execute(db)
     .await?;
     if let Some(_) = state.config.cache {
-        let mut heap = cache.expiries.write();
+        let mut heap = cache.expire_timestamps.write();
         cache.data.insert(key.clone(), contents);
-        heap.push((chrono::offset::Local::now(), key.clone()));
+        heap.push((Local::now(), key.clone()));
     }
-    let mut headers = axum::http::HeaderMap::new();
+    let mut headers = HeaderMap::new();
     headers.insert(
-        axum::http::header::LOCATION,
-        axum::http::header::HeaderValue::from_str(&format!("/{}", key))?,
+        header::LOCATION,
+        HeaderValue::from_str(&format!("/{}", key))?,
     );
-    Ok((
-        axum::http::StatusCode::FOUND,
-        headers,
-        "Paste submitted!".to_string(),
-    ))
+    Ok((StatusCode::FOUND, headers, "Paste submitted!".to_string()))
 }
 
-async fn getpaste(
+async fn get_paste(
     Path(id): Path<String>,
     state: State,
     cache: Arc<Cache>,
-    tera: tera::Tera,
-) -> Result<(axum::http::StatusCode, axum::http::HeaderMap, String), Error> {
+    tera: Tera,
+) -> Result<(StatusCode, HeaderMap, String), Error> {
     let contents: String;
     // TODO replace this with let chaining when rust 1.62 is released
     if let (Some(_), Some(item)) = (state.config.cache, cache.data.get(&id)) {
@@ -207,9 +220,9 @@ async fn getpaste(
         };
         contents = res.contents.ok_or(Error::InternalError)?;
         if let Some(_) = state.config.cache {
-            let mut heap = cache.expiries.write();
+            let mut heap = cache.expire_timestamps.write();
             cache.data.insert(id.clone(), contents.clone());
-            heap.push((chrono::offset::Local::now(), id.clone()));
+            heap.push((Local::now(), id.clone()));
         }
     };
     let mut context = tera::Context::new();
@@ -217,19 +230,16 @@ async fn getpaste(
     context.insert("paste_contents", &contents);
     context.insert("id", &id);
     let final_contents = tera.render("paste.html", &context)?;
-    let mut headers = axum::http::HeaderMap::new();
-    headers.insert(
-        axum::http::header::CONTENT_TYPE,
-        axum::http::header::HeaderValue::from_static("text/html"),
-    );
+    let mut headers = HeaderMap::new();
+    headers.insert(header::CONTENT_TYPE, HeaderValue::from_static("text/html"));
 
-    Ok((axum::http::StatusCode::OK, headers, final_contents))
+    Ok((StatusCode::OK, headers, final_contents))
 }
 
-async fn delete_expired(db: &sqlx::PgPool) {
+async fn delete_expired(db: &PgPool) {
     loop {
         info!("Deleting old pastes...");
-        let now: chrono::DateTime<chrono::Local> = chrono::Local::now();
+        let now: DateTime<Local> = Local::now();
         match query!("DELETE FROM pastes WHERE expires < $1", now)
             .execute(db)
             .await
@@ -237,7 +247,7 @@ async fn delete_expired(db: &sqlx::PgPool) {
             Ok(_) => {}
             Err(e) => tracing::error!("Error deleting expired pastes: {}", e),
         };
-        tokio::time::sleep(std::time::Duration::from_secs(3600)).await;
+        sleep(Duration::from_secs(3600)).await;
     }
 }
 
@@ -252,15 +262,15 @@ async fn clear_cache(cache: Arc<Cache>, max: Option<usize>) {
                 size += item.value().capacity();
             }
             while size > max_size {
-                let heap = cache.expiries.upgradable_read();
+                let heap = cache.expire_timestamps.upgradable_read();
                 if let Some(item) = heap.peek() {
                     size -= item.1.capacity();
                     cache.data.remove(&item.1);
-                    let mut rwheap = parking_lot::RwLockUpgradableReadGuard::upgrade(heap);
-                    rwheap.pop();
+                    let mut heap_rw = parking_lot::RwLockUpgradableReadGuard::upgrade(heap);
+                    heap_rw.pop();
                 }
             }
-            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+            sleep(Duration::from_secs(5)).await;
         }
     }
 }
@@ -271,10 +281,10 @@ enum Error {
     TimeError,
     FieldInvalid,
     InternalError,
-    ToStr(axum::http::header::ToStrError),
-    InvalidHeaderValue(axum::http::header::InvalidHeaderValue),
+    ToStr(header::ToStrError),
+    InvalidHeaderValue(header::InvalidHeaderValue),
     Sqlx(sqlx::Error),
-    Multipart(axum::extract::multipart::MultipartError),
+    Multipart(multipart::MultipartError),
     TemplatingError(tera::Error),
 
     // Errors that might happen to a normal user
@@ -283,8 +293,8 @@ enum Error {
     NotFound,
 }
 
-impl From<axum::http::header::InvalidHeaderValue> for Error {
-    fn from(e: axum::http::header::InvalidHeaderValue) -> Self {
+impl From<header::InvalidHeaderValue> for Error {
+    fn from(e: header::InvalidHeaderValue) -> Self {
         Self::InvalidHeaderValue(e)
     }
 }
@@ -295,8 +305,8 @@ impl From<sqlx::Error> for Error {
     }
 }
 
-impl From<axum::extract::multipart::MultipartError> for Error {
-    fn from(e: axum::extract::multipart::MultipartError) -> Self {
+impl From<multipart::MultipartError> for Error {
+    fn from(e: multipart::MultipartError) -> Self {
         Self::Multipart(e)
     }
 }
@@ -307,43 +317,37 @@ impl From<tera::Error> for Error {
     }
 }
 
-impl From<axum::http::header::ToStrError> for Error {
-    fn from(e: axum::http::header::ToStrError) -> Self {
+impl From<header::ToStrError> for Error {
+    fn from(e: header::ToStrError) -> Self {
         Self::ToStr(e)
     }
 }
 
-impl axum::response::IntoResponse for Error {
-    fn into_response(self) -> axum::response::Response {
-        let (body, status): (std::borrow::Cow<str>, axum::http::StatusCode) = match self {
-            Error::TimeError => ("Bad request".into(), axum::http::StatusCode::BAD_REQUEST),
-            Error::FieldInvalid => (
-                "HTTP field invalid".into(),
-                axum::http::StatusCode::BAD_REQUEST,
-            ),
-            Error::Multipart(_) => (
-                "MultiPartFormData invalid".into(),
-                axum::http::StatusCode::BAD_REQUEST,
-            ),
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        let (body, status): (Cow<str>, StatusCode) = match self {
+            Error::TimeError => ("Bad request".into(), StatusCode::BAD_REQUEST),
+            Error::FieldInvalid => ("HTTP field invalid".into(), StatusCode::BAD_REQUEST),
+            Error::Multipart(_) => ("MultiPartFormData invalid".into(), StatusCode::BAD_REQUEST),
             Error::InternalError => (
                 "Unknown internal error".into(),
-                axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                StatusCode::INTERNAL_SERVER_ERROR,
             ),
             Error::ToStr(_) => (
                 "Error converting header to string".into(),
-                axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                StatusCode::INTERNAL_SERVER_ERROR,
             ),
             Error::InvalidHeaderValue(_) => (
                 "Invalid redirect value (this should be impossible)".into(),
-                axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                StatusCode::INTERNAL_SERVER_ERROR,
             ),
             Error::Sqlx(_) => (
                 "Database lookup failed".into(),
-                axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                StatusCode::INTERNAL_SERVER_ERROR,
             ),
             Error::TemplatingError(_) => (
                 "Templating library error".into(),
-                axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                StatusCode::INTERNAL_SERVER_ERROR,
             ),
             Error::RateLimited(seconds) => (
                 format!(
@@ -351,27 +355,24 @@ impl axum::response::IntoResponse for Error {
                     seconds
                 )
                 .into(),
-                axum::http::StatusCode::TOO_MANY_REQUESTS,
+                StatusCode::TOO_MANY_REQUESTS,
             ),
-            Error::PasteTooLarge => (
-                "Paste too large!".into(),
-                axum::http::StatusCode::TOO_MANY_REQUESTS,
-            ),
+            Error::PasteTooLarge => ("Paste too large!".into(), StatusCode::TOO_MANY_REQUESTS),
             Error::NotFound => (
                 include_str!("./404.html").into(),
-                axum::http::StatusCode::TOO_MANY_REQUESTS,
+                StatusCode::TOO_MANY_REQUESTS,
             ),
         };
-        if status == axum::http::StatusCode::INTERNAL_SERVER_ERROR {
+        if status == StatusCode::INTERNAL_SERVER_ERROR {
             error!("{:#?}", self);
         } else {
             warn!("{:?}", self);
         }
         let body_and_error = include_str!("./error.html").replace("{{ error }}", &body);
-        axum::response::Response::builder()
+        Response::builder()
             .status(status)
             .header("Content-Type", "text/html")
-            .body(axum::body::boxed(axum::body::Full::from(body_and_error)))
+            .body(body::boxed(body::Full::from(body_and_error)))
             .unwrap()
     }
 }
